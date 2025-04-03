5406583003c6fa594b3def696603fe78c0820e79
Oscar Flávio Jr <oscarflaviojunior@gmail.com>
2025-04-03 17:15:58 -0300

    Gerador de senhas aleatórias

---
+++ gerador.rb
@@ -0,0 +1,9 @@
+require 'securerandom'
+
+def criar_senha(tamanho = 12)
+  caracteres = [('a'..'z'), ('A'..'Z'), (0..9), %w[! @ # $ % ^ & * ( ) _ + = -]].map(&:to_a).flatten
+  senha = Array.new(tamanho) { caracteres[SecureRandom.random_number(caracteres.length)] }
+  senha.join
+end
+
+puts "Gerando senha. Por favor, aguarde...\n\nSua senha é:\n\n#{criar_senha}"
